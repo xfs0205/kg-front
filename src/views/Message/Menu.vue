221efbb0e5c3c74b31c63d5fc876376eab6d5d6f
SOso-Soly <liuyisoly@qq.com>
2024-07-08 21:29:31 +0800

    Update Menu.vue

--- src/views/Message/Menu.vue
+++ src/views/Message/Menu.vue
@@ -1,5 +1,11 @@
 <script setup>
 import { menuList } from '@/Message/menu'
+
+const initMenusList = async() => {
+    const res = await menuList()
+    console.log(res)
+}
+initMenusList()
 </script>
 
 <template> 
@@ -23,22 +29,6 @@ import { menuList } from '@/Message/menu'
                 <el-menu-item index="1-4-1">item one</el-menu-item>
             </el-sub-menu>
         </el-sub-menu>
-        <el-menu-item index="2">
-            <el-icon><icon-menu /></el-icon>
-            <span>Navigator Two</span>
-        </el-menu-item>
-        <el-menu-item index="3" disabled>
-            <el-icon>
-                <document />
-            </el-icon>
-            <span>Navigator Three</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-            <el-icon>
-                <setting />
-            </el-icon>
-            <span>Navigator Four</span>
-        </el-menu-item>
     </el-menu>
 
 </template>
